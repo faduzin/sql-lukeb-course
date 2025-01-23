c37d0876658b8addf55ab18e3386c86daba9cddd
faduzin <eric.fadul@gmail.com>
2025-01-23 09:13:33 -0300

    project query 1

---
+++ project_sql/query_1.sql
@@ -0,0 +1,116 @@
+-- 1. What are the top-paying jobs for my role?
+
+-- Quick peek to check what columns and data types we have available
+SELECT
+    column_name,
+    data_type
+FROM
+    information_schema.columns
+WHERE
+    table_name = 'job_postings_fact';
+
+
+SELECT
+    *
+FROM
+    job_postings_fact
+LIMIT 5;
+
+
+SELECT
+    *
+FROM
+    company_dim
+LIMIT 5;
+
+
+-- My solution
+SELECT
+    job_postings_fact.job_title,
+    job_postings_fact.salary_year_avg,
+    company_dim.name as company_name
+FROM
+    job_postings_fact
+    LEFT JOIN company_dim ON company_dim.company_id = job_postings_fact.company_id
+WHERE
+    job_postings_fact.job_title_short = 'Data Analyst' AND
+    job_postings_fact.salary_year_avg IS NOT NULL AND
+    job_postings_fact.job_location = 'Anywhere'
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
+
+
+--Luke's step by step:
+/* Solution query
+Question: What are the top-paying data analyst jobs?
+- Identify the top 10 highest-paying Data Analyst roles that are available remotely.
+- Focus on job postings with specified salaries (remove NULLS).
+- Why? Highlight the top-paying opportunities for Data Analysts, offering insights.
+*/
+-- First Query
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere';
+
+-- Second Query
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL;
+
+-- Third Query
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
+
+-- Fourth Query
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
